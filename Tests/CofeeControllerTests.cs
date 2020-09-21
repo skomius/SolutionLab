6a2d0550d0de7669732107a5fa0acbd11c4d2014
skomius <skomantas.juskys@outlook.com>
2020-09-21 07:36:11 +0300

    controller test fix

--- Tests/CofeeControllerTests.cs
+++ Tests/CofeeControllerTests.cs
@@ -5,22 +5,24 @@ using System.Threading.Tasks;
 public class CofeeControllerTests
 {
     [Test]
-    public void CoffeeController_Get_ShouldReturnCoffee()
+    public async Task CoffeeController_Get_ShouldReturnCoffee()
     {
         var mock = new Mock<IRepository<Coffee>>();
-        mock.Setup(p => p.Get(1)).Returns(new Task<Coffee>(() => new Coffee() {
+   
+        mock.Setup(p => p.Get(It.IsAny<int>())).Returns(Task.FromResult<Coffee>(new Coffee()
+        {
             Id = 1,
             Name = "Test",
             Price = 1,
             PicUrl = "Test"
         }));
 
-        var controller = new CoffeeController(mock.Object as CoffeeRepository);
-        var result = controller.Get(1).Result.Value;
+        var controller = new CoffeeController(mock.Object);
+        var result = await controller.Get(1);
 
-        Assert.AreEqual(result.Id, 1);
-        Assert.AreEqual(result.Name, "Test");
-        Assert.AreEqual(result.Price, 1);
-        Assert.AreEqual(result.PicUrl, "Test");
+        Assert.AreEqual(result.Value.Id, 1);
+        Assert.AreEqual(result.Value.Name, "Test");
+        Assert.AreEqual(result.Value.Price, 1);
+        Assert.AreEqual(result.Value.PicUrl, "Test");
     }
 }
